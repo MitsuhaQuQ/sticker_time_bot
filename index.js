e43f5bc23652e1adff54663cf37326c51316f711
Mitsuha_QuQ <51279729+MitsuhaQuQ@users.noreply.github.com>
2019-06-03 11:12:35 +0800

    Add files via upload
    Rewrite cron job and add 6:9:08 timer

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const logger = createLogger({
 const token = config.tg_bot_token;
 
 const sticker1040 = 'CAADBQADAQAD9tu2MpYEjqu_M70iAg' ;
+conse sticker698 = 'CAADBQADAgAD9tu2MpKuO-JFuzpkAg' ;
 
 const bot = new TelegramBot(token, { polling: true });
 
@@ -101,6 +102,11 @@ bot.onText(/\/ping/, (msg) => {
 	bot.sendVoice(chatId, 'ATC_Ding.ogg');
 });
 
+bot.onText(/\/test/, (msg) => {
+	const chatId = msg.chat.id;
+	bot.sendMessage(chatId, '请勿打扰司机驾驶！');
+});
+
 bot.onText(/\/trainnow/, (msg) => {
 	const chatId = msg.chat.id;
 	bot.sendMessage(chatId, '加开列车！');
@@ -121,88 +127,52 @@ bot.on('webhook_error', (error) => {
     logger.error('[webhook_error] ' + error.code);  // => 'EPARSE'
 });
 
-var cron = new CronJob('38 10,22 * * *', function() {
-	var date = new Date();
-    data.chatids.forEach(function (id) {
-        let tz = data.tzmap[id];
-        if (!tz) {
-            tz = 'Asia/Shanghai';
-        }
-		
-		logger.debug('Send prepar ' + id);
-		bot.sendVoice(chatId, 'ATOS.ogg');
-		bot.sendMessage(id, '旅客们请注意，开往 北部湾 方向的 高1040次列车即将到达本站');
-		bot.sendMessage(id, '列车到达 9 站台');
+var cronJob = require("cron").CronJob; 
+
+new cronJob('0 8 6,18 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send prepar ' + id);
+	bot.sendVoice(id, 'ATOS.ogg');
+	bot.sendMessage(id, '旅客们请注意，开往 北部湾 方向的 D698次列车即将到达本站');
+	bot.sendMessage(id, '列车到达 9 站台');
 	});
 }, null, true, 'Asia/Shanghai');
 
-// when 10:40 or 22:40 send a sticker
-var cron = new CronJob('40 10,22 * * *', function() {
-    var date = new Date();
-    logger.info('Cron triggered: ' + date + ', send sticker to ' + data.chatids.length + ' chats');
-    data.chatids.forEach(function (id) {
-        let tz = data.tzmap[id];
-        if (!tz) {
-            tz = 'Asia/Shanghai';
-        }
-
-        logger.debug('Send to ' + id);
-        bot.sendSticker(id, sticker1040 ).then(message => {
-            let cid = message.chat.id;
-            let mid = message.message_id;
-            if (data.autodelete[cid] && data.lastid[cid]) {
-                bot.deleteMessage(cid, data.lastid[cid]);
-            }
-            data.lastid[cid] = mid;
-            saveData();
-        }).catch(error => {
-            let query = error.response.request.uri.query;
-            let matches = query.match(/chat_id=(.*)&/);
-            if (matches && matches[1]) {
-                let cid = Number(matches[1]);
-                if (isNaN(cid)) {
-                    // Channel name
-                    cid = matches[1];
-                    cid = cid.replace('%40', '@');
-                }
-                logger.error('[' + error.response.body.error_code + '](' + cid + ')' + error.response.body.description);  // => 'ETELEGRAM'
-                if (query && (error.response.body.error_code === 403 || error.response.body.error_code === 400) &&
-                (error.response.body.description.includes('blocked') ||
-                    error.response.body.description.includes('kicked') ||
-                    error.response.body.description.includes('not a member') ||
-                    error.response.body.description.includes('chat not found') ||
-                    error.response.body.description.includes('upgraded') ||
-                    error.response.body.description.includes('deactivated') ||
-                    error.response.body.description.includes('not enough rights') ||
-                    error.response.body.description.includes('have no rights') ||
-                    error.response.body.description.includes('CHAT_SEND_STICKERS_FORBIDDEN'))) {
-                    logger.info('Blocked by ' + cid);
-                    let index = data.chatids.indexOf(cid);
-                    if (index > -1) {
-                        data.chatids.splice(index, 1);
-                        delete data.tzmap[cid];
-                        delete data.lastid[cid];
-                        delete data.autodelete[cid];
-                        delete data.sleeptime[cid];
-                        delete data.waketime[cid];
-                        saveData();
-                    }
-                }
-            }
-        })
-    });
+new cronJob('8 9 6,18 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send to ' + id);
+    bot.sendSticker(id, sticker698 );
+	});
 }, null, true, 'Asia/Shanghai');
 
-var cron = new CronJob('41 10,22 * * *', function() {
-	var date = new Date();
-    data.chatids.forEach(function (id) {
-        let tz = data.tzmap[id];
-        if (!tz) {
-            tz = 'Asia/Shanghai';
-        }
-		
-		logger.debug('Send end ' + id);
-		bot.sendMessage(id, '列车关门，请乘客们注意安全');
-		bot.sendVoice(chatId, 'departure.ogg');
+new cronJob('0 10 6,18 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send end ' + id);
+	bot.sendMessage(id, '列车关门，请乘客们注意安全');
+	bot.sendVoice(id, 'departure.ogg');
+	});
+}, null, true, 'Asia/Shanghai');
+
+new cronJob('0 38 10,22 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send prepar ' + id);
+	bot.sendVoice(id, 'ATOS.ogg');
+	bot.sendMessage(id, '旅客们请注意，开往 北部湾 方向的 G1040次列车即将到达本站');
+	bot.sendMessage(id, '列车到达 9 站台');
+	});
+}, null, true, 'Asia/Shanghai');
+
+new cronJob('0 40 10,22 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send to ' + id);
+    bot.sendSticker(id, sticker1040 );
+	});
+}, null, true, 'Asia/Shanghai');
+
+new cronJob('0 41 10,22 * * *', function(){
+	data.chatids.forEach(function (id){
+	logger.debug('Send end ' + id);
+	bot.sendMessage(id, '列车关门，请乘客们注意安全');
+	bot.sendVoice(id, 'departure.ogg');
 	});
 }, null, true, 'Asia/Shanghai');
